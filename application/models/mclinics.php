a9dceddd35fd3896dd37da650ba54d5486568b46
maricel prestoza <mymiracle91@gmail.com>
2016-02-04 16:57:12 -0500

    version 6

--- application/models/mclinics.php
+++ application/models/mclinics.php
@@ -26,7 +26,34 @@ class mclinics extends CI_Model {
 		$serviceid = $this->input->post('serviceid');
 		$clinicid = $this->input->post('clinicid');
 		$userid = $this->session->userdata('userid');
-		$data = array('service_id'=>$serviceid,'clinic_id'=>$clinicid,'client_id'=>$userid);
-		$this->db->insert('bookmark');
+		//get list of bookmarked and verify if the serviceid already exist
+		$this->db->where('client_id',$userid);
+		$this->db->select('service_id');
+		$q = $this->db->get('bookmark');
+		if($q->num_rows() > 0){
+			$row = $q->row();
+			$services = explode(',',$row->service_id);
+			if(in_array($serviceid,$services)){
+				return 1; //already bookmarked
+				exit();
+			}else{
+				array_push($services,$service);
+				$this->db->where('client_id',$userid);
+				$d = $this->db->update('bookmark',array('service_id'=>implode($services)));
+				if($d == true)
+					return 2;//bookmark updated
+				else return 0; //error occurred
+				exit();
+			}
+
+		}else{
+			$data = array('service_id'=>$serviceid,'clinic_id'=>$clinicid,'client_id'=>$userid);
+			$d = $this->db->insert('bookmark');
+
+			if($d == true)
+				return 3: //new service bookmark
+			else return 0; //error occurred
+			exit();
+		}
 	}
 }
